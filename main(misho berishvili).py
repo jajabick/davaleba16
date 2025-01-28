96f970de2cfbd6473875ed5dc67654e634983213
jajabick <mishka.mishka.mlg@gmail.com>
2025-01-28 19:28:43 +0400

    Updated code

---
+++ main(misho berishvili).py
@@ -0,0 +1,25 @@
+class student:
+    
+    def __init__(self, student_data):
+        self.student_data = student_data
+        
+    def __str__(self):
+        
+        result = ""
+        for key, value in self.student_data.items():
+            result += f"{key}: {value}"
+        return result
+
+student_info = {"name " : "nika ", "lastname " : "gvariani ", "age " : 16, " personal number " : 19528551943, " grades " : "10 ", " time until 18 ": 18 - 16}
+student1 = student(student_info)
+print(student1)
+
+student_info2 = {"name " : "ani ", "lastname " : "maisuradze ", "age " : 13, " personal number " : 80194752043, " grades " : "7 ", " time until 18 ": 18 - 13}
+student2 = student(student_info2)
+print(student2)
+
+
+student_info3 = {"name " : "gio ", "lastname " : "davitashvili ", "age " : 17, " personal number " : 62375991738, " grades " : "3 ", " time until 18 ": 18 - 17}
+
+student3 = student(student_info3)
+print(student3)
